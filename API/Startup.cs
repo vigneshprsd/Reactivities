1b367fbf339df9e385143fbf093523dc0a0e746f
vigneshprsd <vigneshprsd@gmail.com>
2020-09-05 13:46:31 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -30,6 +30,11 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy",policy =>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -43,6 +48,8 @@ namespace API
 
            // app.UseHttpsRedirection();
 
+            app.UseCors("CorsPolicy");
+            
             app.UseRouting();
 
             app.UseAuthorization();
@@ -51,6 +58,7 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+            
         }
     }
 }
